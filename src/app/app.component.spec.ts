0da6d490c9abc5f6d2ab947660a1352cd342aa39
sfischer-net <115459460+sfischer-net@users.noreply.github.com>
2022-10-10 15:03:34 +0200

    init github pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'sfischer-net'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sfischer-net');
+    expect(app.title).toEqual('s-fischer.net');
   });
 
   it('should render title', () => {
